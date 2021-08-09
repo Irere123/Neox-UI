1a214576393ec4e213dbbe1dbaa0362756dad8be
irere emmanuel <irere2050@gmail.com>
2021-08-10 13:17:45 +1400

    Changes

--- src/components/kousa/Message.jsx
+++ src/components/kousa/Message.jsx
@@ -1,29 +1,29 @@
-import React from 'react';
+import React from "react";
 
-import RenderText from '../RenderText';
-import '../../styles/kousa/Message.css';
+import RenderText from "../RenderText";
+import "../../styles/kousa/Message.css";
 
 const Message = ({ message: { url, text, filetype } }) => {
   if (url) {
-    if (filetype.startsWith('image/')) {
+    if (filetype.startsWith("image/")) {
       return (
-        <div className='image-message'>
-          <img src={url} alt='' />
+        <div className="image-message">
+          <img src={url} alt="" />
         </div>
       );
-    } else if (filetype === 'text/plain') {
+    } else if (filetype === "text/plain") {
       return <RenderText url={url} />;
-    } else if (filetype.startsWith('audio/')) {
+    } else if (filetype.startsWith("audio/")) {
       return (
-        <div className='audio-message'>
+        <div className="audio-message">
           <audio controls>
             <source src={url} />
           </audio>
         </div>
       );
-    } else if (filetype.startsWith('video/')) {
+    } else if (filetype.startsWith("video/")) {
       return (
-        <div className='video-message'>
+        <div className="video-message">
           <video controls src={url}></video>
         </div>
       );
