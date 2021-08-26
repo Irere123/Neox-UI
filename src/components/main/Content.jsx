221f42fe9361819adb8614e87dffaa93316472d9
irere123 <irere2050@gmail.com>
2021-08-27 03:43:06 +1400

    feature: added content cards

---
+++ src/components/main/Content.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { Avatar } from "@material-ui/core";
+import { Comment, Favorite, Share } from "@material-ui/icons";
+
+import "../../styles/MainPage.css";
+
+function Content({ username, createdAt }) {
+  return (
+    <>
+      <div className="ContentCard__maiPage">
+        <div className="ContentCard__maiPage_header">
+          <Avatar>HJ</Avatar>
+          <div className="ContentCard__mainPage">
+            <h3>{username}</h3>
+            <h4>{createdAt}</h4>
+          </div>
+        </div>
+        <div className="ContentCard__maiPage_content">
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum
+            perferendis quod quibusdam facere nulla, eius error reprehenderit
+            similique. Reiciendis obcaecati veniam dolor, ratione aut magni
+            officiis illo dicta dolorum eveniet!
+          </p>
+        </div>
+        <div className="ContentCard__maiPage_footer">
+          <div className="ContentCard__maiPage_footer_icon_word">
+            <Favorite />
+            <h4>Like</h4>
+          </div>
+          <div className="ContentCard__maiPage_footer_icon_word">
+            <Comment />
+            <h4>Comment</h4>
+          </div>
+          <div className="ContentCard__maiPage_footer_icon_word">
+            <Share />
+            <h4>Share</h4>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Content;
--- src/containers/MainPageContainer.jsx
+++ src/containers/MainPageContainer.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 
+import "../styles/MainPage.css";
 import Header from "../components/main/Header";
+import PostCard from "../components/main/Content";
 
 function MainPageContainer() {
   return (
     <div>
       <Header />
+      <div className="MainPage__content__container">
+        <div className="posts">
+          <PostCard
+            createdAt="created on July 2021"
+            username="Mark Zuckerberg"
+          />
+          <PostCard createdAt="created on June 2021" username="Surge SH" />
+        </div>
+      </div>
     </div>
   );
 }
